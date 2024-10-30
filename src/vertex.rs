c30680722ff47c3e16c28828190d5ed083f1fd7e
Exotik850 <kidkool850@gmail.com>
2024-10-30 11:47:52 -0500

    flip image vertices

--- src/vertex.rs
+++ src/vertex.rs
@@ -8,10 +8,10 @@ pub struct Vertex {
 }
 
 pub const QUAD: [Vertex; 4] = [
-    Vertex::from_x_y(-1.0, -1.0, 0.0, 0.0),  // Bottom left
-    Vertex::from_x_y(1.0, -1.0, 1.0, 0.0),   // Bottom right
-    Vertex::from_x_y(-1.0, 1.0, 0.0, 1.0),   // Top left
-    Vertex::from_x_y(1.0, 1.0, 1.0, 1.0),    // Top right
+    Vertex::from_x_y(-1.0, -1.0, 0.0, 1.0),  // Bottom left
+    Vertex::from_x_y(1.0, -1.0, 1.0, 1.0),   // Bottom right
+    Vertex::from_x_y(-1.0, 1.0, 0.0, 0.0),   // Top left
+    Vertex::from_x_y(1.0, 1.0, 1.0, 0.0),    // Top right
 ];
 pub const QUAD_INDICES: [u16; 4] = [0, 1, 2, 3];
 
